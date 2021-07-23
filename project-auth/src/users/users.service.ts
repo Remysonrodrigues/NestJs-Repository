a56c3bedc49fd3b73220c30cca97bd4d13f0c921
remyson <remysonrodrigues@gmail.com>
2021-07-23 15:02:48 -0300

    add logic services auth and user

--- project-auth/src/users/users.service.ts
+++ project-auth/src/users/users.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private readonly users = [
+        {
+            userId: 1,
+            username: 'jhon',
+            password: 'changeme'
+        },
+        {
+            userId: 2,
+            username: 'maria',
+            password: 'guess'
+        }
+    ];
+
+    async findOne(username: string): Promise<any | undefined> {
+        return this.users.find(user => user.username === username);
+    }
+}
