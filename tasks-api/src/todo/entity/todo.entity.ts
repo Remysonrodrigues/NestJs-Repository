8c51eb185e94a875cab16f69f6e8296814fe4258
remyson <remysonrodrigues@gmail.com>
2021-08-06 16:46:56 -0300

    add task entity

--- tasks-api/src/todo/entity/todo.entity.ts
+++ tasks-api/src/todo/entity/todo.entity.ts
@@ -1,5 +1,24 @@
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { TaskEntity } from '@todo/entity/task.entity';
+
+@Entity('todo')
 export class TodoEntity {
+    
+    @PrimaryGeneratedColumn('uuid')
     id: string;
+
+    @Column({ type: 'varchar', nullable: false })
     name: string;
+    
+    @Column({ type: 'text', nullable: true })
     description?: string;
+
+    @CreateDateColumn()
+    createdOn?: Date;
+
+    @CreateDateColumn()
+    updatedOn?: Date;
+
+    @OneToMany(type => TaskEntity, task => task.todo)
+    tasks?: TaskEntity[];
 }
