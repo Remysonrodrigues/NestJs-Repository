6c2834ca82858fba8b31c4d7d6023b63e326ce60
remyson <remysonrodrigues@gmail.com>
2021-08-10 10:40:49 -0300

    refactor todo module

--- tasks-api/src/todo/todo.module.ts
+++ tasks-api/src/todo/todo.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
+import { UserEntity } from 'src/users/entity/user.entity';
+import { UsersModule } from 'src/users/users.module';
 import { TaskEntity } from './entity/task.entity';
 import { TodoEntity } from './entity/todo.entity';
 import { TaskController } from './task/task.controller';
@@ -9,7 +12,9 @@ import { TodoService } from './todo.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([TodoEntity, TaskEntity])
+    UsersModule,
+    AuthModule,
+    TypeOrmModule.forFeature([TodoEntity, TaskEntity, UserEntity])
   ],
   controllers: [TodoController, TaskController],
   providers: [TodoService, TaskService]
