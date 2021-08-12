bcea883aaec765f56fb05287838cdeaca162e770
remyson <remysonrodrigues@gmail.com>
2021-08-12 10:03:53 -0300

    add auth module

--- nestjs-pgsql-api/src/app.module.ts
+++ nestjs-pgsql-api/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './configs/ typeorm.config';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig), 
-    UsersModule
+    UsersModule, AuthModule
   ],
   controllers: [],
   providers: [],
