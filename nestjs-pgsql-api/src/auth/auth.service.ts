ffbac25b9964e99ccd04df653c58fea65c62c142
remyson <remysonrodrigues@gmail.com>
2021-08-13 10:51:56 -0300

    refactor auth service

--- nestjs-pgsql-api/src/auth/auth.service.ts
+++ nestjs-pgsql-api/src/auth/auth.service.ts
@@ -10,6 +10,7 @@ import { User } from '../users/user.entity';
 import { UserRole } from '../users/enums/user-roles.enum';
 import { CredentialsDto } from './dto/credentials.dto';
 import { JwtService } from '@nestjs/jwt';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AuthService {
@@ -17,13 +18,28 @@ export class AuthService {
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
     private jwtService: JwtService,
+    private mailerService: MailerService,
   ) {}
 
   async signUp(createUserDto: CreateUserDto): Promise<User> {
     if (createUserDto.password != createUserDto.passwordConfirmation) {
       throw new UnprocessableEntityException('As senhas não conferem');
     } else {
-      return await this.userRepository.createUser(createUserDto, UserRole.USER);
+      const user = await this.userRepository.createUser(
+        createUserDto,
+        UserRole.USER,
+      );
+      const mail = {
+        to: user.email,
+        from: 'noreply@application.com',
+        subject: 'Email de confirmação',
+        template: 'email-confirmation',
+        context: {
+          token: user.confirmationToken,
+        },
+      };
+      await this.mailerService.sendMail(mail);
+      return user;
     }
   }
 
