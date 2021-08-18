8ab6882a6619c0846b5c745da168b66555ebd6c7
remyson <remysonrodrigues@gmail.com>
2021-08-18 14:28:01 -0300

    refactor dotenv module

--- nestjs-pgsql-api/src/configs/typeorm.config.ts
+++ nestjs-pgsql-api/src/configs/typeorm.config.ts
@@ -1,11 +1,14 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  username: String(process.env.DB_USER),
+  password: String(process.env.DB_PASSWORD),
   database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: Boolean(process.env.DB_SYNCHRONIZE),
